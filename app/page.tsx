214c52063589175129ff9ec5a974ea4da01dd2b5
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-11-27 12:32:21 +0100

    feat: show cpu load as chart

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { OsInfo } from "@/components/os-info";
 export default function Home() {
   return (
     <DeviceInfoProvider interval={Number(process.env.UPDATE_INTERVAL) ?? 1000}>
-      <main className="min-h-[100vh] flex flex-col justify-center p-4 sm:p-8">
+      <main className="flex min-h-[100vh] flex-col p-4 sm:p-8">
         <div>
           <h3 className="text-lg font-bold tracking-tighter">CPU</h3>
           <CPUInfo />
--- components/cpu-info.tsx
+++ components/cpu-info.tsx
@@ -1,15 +1,65 @@
 "use client";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 import { useDeviceInfo } from "./contexts/device-info/device-info-context";
 
-export function CPUInfo() {
+type CPUInfoProps = {
+  barRadius?: number;
+};
+
+const chartConfig = {
+  cpu: { color: "49 155 145" },
+} satisfies ChartConfig;
+
+export function CPUInfo({ barRadius = 3 }: CPUInfoProps) {
   const { cpus } = useDeviceInfo();
+  const chartData = cpus.map(({ usage }) => ({
+    usage,
+    available: 100 - usage,
+  }));
 
   return (
-    <ul>
-      {cpus.map((cpu, index) => (
-        <CPU key={index} info={cpu} />
-      ))}
-    </ul>
+    <div className="relative overflow-hidden pl-4">
+      <ChartContainer config={chartConfig} className="w-full max-w-screen-sm">
+        <BarChart
+          className="relative"
+          accessibilityLayer
+          data={chartData}
+          layout="vertical"
+          barSize={50}
+        >
+          <Bar
+            layout="vertical"
+            stackId={0}
+            dataKey="usage"
+            fill="rgb(var(--color-cpu))"
+            radius={[barRadius, 0, 0, barRadius]}
+          />
+          <Bar
+            layout="vertical"
+            stackId={0}
+            dataKey="available"
+            fill="rgb(var(--color-cpu)/0.3)"
+            radius={[0, barRadius, barRadius, 0]}
+          />
+
+          <YAxis dataKey="usage" type="category" hide />
+          <XAxis type="number" dataKey="usage" domain={[0, 100]} hide />
+        </BarChart>
+      </ChartContainer>
+      <div className="absolute inset-0 grid items-center py-1 pl-8 font-mono text-xs text-[rgb(206_100_110)] mix-blend-difference md:text-sm">
+        {cpus.map(({ usage }, index) => (
+          <span key={index}>{Math.round(usage * 10) / 10}</span>
+        ))}
+      </div>
+      <div className="absolute inset-0 grid w-8 items-center justify-end px-4 py-1 font-mono text-xs text-muted-foreground md:text-sm">
+        {cpus.map((_, index) => (
+          <span key={index} className="text-end">
+            {index + 1}
+          </span>
+        ))}
+      </div>
+    </div>
   );
 }
 
