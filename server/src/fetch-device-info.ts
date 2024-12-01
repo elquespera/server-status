c545cd65a923ea4b6f2db67cdaa4dae672c032c9
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-12-01 16:51:53 +0100

    fix: do not using exec for testing

--- server/src/fetch-device-info.ts
+++ server/src/fetch-device-info.ts
@@ -26,32 +26,36 @@ export async function fetchDeviceInfo(
   let battery: TermuxBattery | undefined = previousInfo?.battery;
   let wifi: TermuxWifiInfo | undefined = previousInfo?.wifi;
 
-  try {
-    if (sudo) {
-      const { stdout: cpuRaw } = await exec(
-        `sudo node -e "console.log(JSON.stringify(os.cpus()))"`,
-      );
-      cpus = JSON.parse(cpuRaw);
-    } else {
-      cpus = cpuInfo();
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  // try {
+  //   if (sudo) {
+  //     const { stdout: cpuRaw } = await exec(
+  //       `sudo node -e "console.log(JSON.stringify(os.cpus()))"`,
+  //     );
+  //     cpus = JSON.parse(cpuRaw);
+  //   } else {
+  //     cpus = cpuInfo();
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
 
-  if (isTermux) {
-    try {
-      const { stdout: wifiRaw } = await exec("termux-wifi-connectioninfo");
-      wifi = JSON.parse(wifiRaw);
-      const { stdout: batteryRaw } = await exec("termux-battery-status");
-      battery = JSON.parse(batteryRaw);
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    wifi = mockWifiInfo;
-    battery = mockBattery;
-  }
+  cpus = cpuInfo();
+  wifi = mockWifiInfo;
+  battery = mockBattery;
+
+  // if (isTermux) {
+  //   try {
+  //     const { stdout: wifiRaw } = await exec("termux-wifi-connectioninfo");
+  //     wifi = JSON.parse(wifiRaw);
+  //     const { stdout: batteryRaw } = await exec("termux-battery-status");
+  //     battery = JSON.parse(batteryRaw);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // } else {
+  //   wifi = mockWifiInfo;
+  //   battery = mockBattery;
+  // }
 
   if (cpus)
     return {
