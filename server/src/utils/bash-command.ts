fcc685a9811163939216d9a4555a6f738ae70120
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2025-01-12 13:38:50 +0100

    fix: increase max buffer for bash command execution

--- server/src/utils/bash-command.ts
+++ server/src/utils/bash-command.ts
@@ -2,6 +2,10 @@ import { exec as execSync } from "child_process";
 import util from "node:util";
 
 const exec = util.promisify(execSync);
+const maxBuffer = 10 * 1024 * 1024;
 
 export const runBashCommand = async (command: string) =>
-  exec(command, { shell: "/data/data/com.termux/files/usr/bin/bash" });
+  exec(command, {
+    shell: "/data/data/com.termux/files/usr/bin/bash",
+    maxBuffer,
+  });
